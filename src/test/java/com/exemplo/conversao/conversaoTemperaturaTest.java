8c1c1154196018aab6c056d94b0adeb68b4fbebe
Matheus Gab <154209152+mat-g10@users.noreply.github.com>
2024-07-19 22:31:47 -0300

    Create conversaoTemperaturaTest.java

---
+++ src/test/java/com/exemplo/conversao/conversaoTemperaturaTest.java
@@ -0,0 +1,19 @@
+package com.exemplo.conversao;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ConversaoTemperaturaTest {
+
+    @Test
+    public void testFahrenheitParaCelsius() {
+        assertEquals(0.0, ConversaoTemperatura.fahrenheitParaCelsius(32.0), 0.01);
+        assertEquals(100.0, ConversaoTemperatura.fahrenheitParaCelsius(212.0), 0.01);
+    }
+
+    @Test
+    public void testCelsiusParaFahrenheit() {
+        assertEquals(32.0, ConversaoTemperatura.celsiusParaFahrenheit(0.0), 0.01);
+        assertEquals(212.0, ConversaoTemperatura.celsiusParaFahrenheit(100.0), 0.01);
+    }
+}
